be41dcbbe4d1330ee7130d2d4fa0660e43828bfc
igor26111963 <poltorak63@mail.ru>
2021-02-05 06:31:36 +0300

    первые функции

--- js/main.js
+++ js/main.js
@@ -3,19 +3,19 @@
 //  } Функция возвращающая случайное целое число из заданого интервала.
 
 const random = (a, b) => {
-	let minNumber = a; 
-	let maxNumber = b;
-	if (minNumber >= maxNumber) {
+  let minNumber = a; 
+  let maxNumber = b;
+  if (minNumber >= maxNumber) {
 		minNumber = b;
-      maxNumber = a;
-	}
-	if (minNumber >= 0 && maxNumber > 0) {
-		return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-		}
-	return null; 
+		maxNumber = a;
+  }
+  if (minNumber >= 0 && maxNumber > 0) {
+	  return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
 }
-let c = random (5, 10);
-console.log(c);
+  return null; 
+}
+// let c = random (5, 10);
+// console.log(c);
 
 
 // функция возвращающая случайное число с плавающей запятой из заданого интенвала.  
@@ -25,12 +25,12 @@ const randomToFixed = (a, b) => {
 	let maxNumber = b;
 	if (minNumber >= maxNumber) {
 		minNumber = b;
-      maxNumber = a;
+		maxNumber = a;
 	}
 	if (minNumber >= 0 && maxNumber > 0) {
 		return ((Math.random() * (maxNumber - minNumber + 1)) + minNumber).toFixed (3);
 		}
 	return null;
 }
-let d = randomToFixed (4, 10);
-console.log(d);
+// let d = randomToFixed (4, 10);
+// console.log(d);
