720ed4f0aedc0011edbc6ae73577c09079f748d2
igor26111963 <poltorak63@mail.ru>
2021-02-05 10:27:49 +0300

    самые первые функции

--- js/main.js
+++ js/main.js
@@ -1,29 +1,29 @@
 const random = (a, b) => {
-   let minNumber = a;
-   let maxNumber = b;
-   if (minNumber >= maxNumber) {
-      minNumber = b;
-      maxNumber = a;
-   }
-   if (minNumber >= 0 && maxNumber > 0) {
-      return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
-   }
-   return null;
+  let minNumber = a;
+  let maxNumber = b;
+  if (minNumber >= maxNumber) {
+    minNumber = b;
+    maxNumber = a;
+  }
+  if (minNumber >= 0 && maxNumber > 0) {
+    return Math.floor(Math.random() * (maxNumber - minNumber + 1)) + minNumber;
+  }
+  return null;
 }
 let b = random(5, 10);
 console.log(b);
 
 const randomToFxed = (a, b) => {
-   let minNumber = a;
-   let maxNumber = b;
-   if (minNumber >= maxNumber) {
-      minNumber = b;
-      maxNumber = a;
-   }
-   if (minNumber >= 0 && maxNumber > 0) {
-      return ((Math.random() * (maxNumber - minNumber + 1)) + minNumber).toFixed(3);
-   }
-   return null;
+  let minNumber = a;
+  let maxNumber = b;
+  if (minNumber >= maxNumber) {
+    minNumber = b;
+    maxNumber = a;
+  }
+  if (minNumber >= 0 && maxNumber > 0) {
+    return ((Math.random() * (maxNumber - minNumber + 1)) + minNumber).toFixed(3);
+  }
+  return null;
 }
 let a = random(5, 10)
 console.log(a);
