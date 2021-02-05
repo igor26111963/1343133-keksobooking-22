22c61f288f803d874f3d2eaebcef27957ef6649e
igor26111963 <poltorak63@mail.ru>
2021-02-05 17:31:34 +0300

    первые функции

--- js/main.js
+++ js/main.js
@@ -11,19 +11,20 @@ const random = (a, b) => {
   return null;
 }
 let b = random(5, 10);
-console.log(b);
+// console.log(b);
 
-const randomToFixed = (a, b) => {
+const randomToFixed = (a, b, c) => {
   let minNumber = a;
   let maxNumber = b;
+  let number_of_signs = c;
   if (minNumber >= maxNumber) {
     minNumber = b;
     maxNumber = a;
   }
   if (minNumber >= 0 && maxNumber > 0) {
-    return ((Math.random() * (maxNumber - minNumber + 1)) + minNumber).toFixed(3);
+    return ((Math.random() * (maxNumber - minNumber + 1)) + minNumber) * Math.pow(10, number_of_signs) / Math.floor(Math.pow(10, number_of_signs));
   }
   return null;
 }
-let a = randomToFixed(5, 10)
-console.log(a);
+let a = randomToFixed(5, 10, 4)
+// console.log(a);
